ef725b1613b5e34843e8afbec2032747a1969415
KoreanCow <vue-cli@kakao.com>
2024-09-09 17:23:03 +0900

    feat: add Metadata in User's Page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,24 @@
 'use client';
 import Image from "next/image";
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 import styles from '../page.module.scss';
 import search from '@/../public/images/icons/search.png';
 
 export default function Home() {
-
   const [nickname, setNickname] = useState('');
+  const router = useRouter();
 
   const onSearch = async (e?: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLImageElement, MouseEvent>) => {
     e?.preventDefault();
+
+    const trimNickname = nickname.trim();
+
+    if (trimNickname !== '') {
+      router.push(`/users/${trimNickname}`)
+      setNickname('');
+    }
     console.log(nickname);
   };
 
---
+++ src/app/users/[nickname]/layout.tsx
@@ -0,0 +1,23 @@
+import { Metadata } from 'next';
+
+export async function generateMetadata(
+  { params }: { params: { nickname: string } },
+): Promise<Metadata> {
+  const decodedNickname = decodeURI(params.nickname);
+  return {
+    title: `${decodedNickname}'s Profile`,
+    description: `Profile page for user: ${decodedNickname}`
+  }
+}
+
+export default function UserLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
