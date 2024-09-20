2da9d22ab01551dd076c231fefe7ed8dfa93f536
KoreanCow <vue-cli@kakao.com>
2024-09-20 14:32:57 +0900

    refactor: importing Types

--- src/app/users/[nickname]/page.tsx
+++ src/app/users/[nickname]/page.tsx
@@ -1,19 +1,20 @@
 'use client';
 
+import { UserNumState } from '@/types/user/info';
 import { useUserNum } from '../../../../hooks/useUserNum';
 import UserInfo from '../components/UserInfo';
-
+import UserStat from '../components/UserStat';
 export default function UserPage({ params }: { params: { nickname: string } }) {
   const decodedNickname = decodeURI(params.nickname);
-  const { userData, loading, error } = useUserNum(decodedNickname);
+  const userNumState: UserNumState = useUserNum(decodedNickname);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
-  if (!userData) return <p>User not found</p>; // userData가 null일 때 처리
 
   return (
     <>
-      <UserInfo userNum={userData} /> {/* userData가 null이 아님을 보장 */}
+      <h1>User Info</h1>
+
+      <UserInfo userNumState={userNumState} />
+      <UserStat userNumState={userNumState} />
     </>
   );
 }
