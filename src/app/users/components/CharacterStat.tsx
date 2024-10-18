c35b6058a964833652bc21f425d8dbc2b371dc56
KoreanCow <vue-cli@kakao.com>
2024-10-18 18:17:22 +0900

    feat: Add hooks that uses characterCode

--- src/app/users/components/CharacterStat.tsx
+++ src/app/users/components/CharacterStat.tsx
@@ -1,16 +1,26 @@
 import type { CharacterStat } from '@/types/user/stat';
 import styles from '../[nickname]/nickname.module.scss'
-
+import { useCharacterInfo } from '../../../../hooks/useCharacter';
+import Image from 'next/image';
 interface CharacterStatProps {
   characterStat: CharacterStat[];
 }
 
+
 export default function CharacterStat({ characterStat }: CharacterStatProps) {
+
   return (
     <div className={styles.character}>
       {characterStat.map(stat => (
         <div key={stat.characterCode} className={styles.character_item}>
+          <Image
+            src={`/images/characters/${useCharacterInfo(stat.characterCode).characterInfo?.name}.png`}
+            alt='character img'
+            width={100}
+            height={100}
+          />
           <p>Character Code: {stat.characterCode}</p>
+          <p>{useCharacterInfo(stat.characterCode).characterInfo?.name}</p>
           <p>Total Games: {stat.totalGames}</p>
           <p>Usages: {stat.usages}</p>
           <p>Max Killings: {stat.maxKillings}</p>
